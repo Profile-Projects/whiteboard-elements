053bd35813d355e9f4059e7702a2e29093c3a272
santhosh <santhoshg1990@gmail.com>
2024-01-10 23:51:54 +0530

    wip

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 const express = require('express');
 const http = require('http');
-const {Server} = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
+const { Server } = require('socket.io');
 
-const io = new Server(server);
+const cors = require('cors');
+
+const corsOptions = {
+    origin: '*', // This allows requests from any origin.
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // This allows the specified methods.
+    headers: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'], // This allows the specified headers.
+  };
+  app.use(cors(corsOptions));
+
+// IO.set('origin',":*:");
+
+// const { Server } = io;
+
+const socketIO = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
+    },
+    log:{
+        level: 1
+    }
+});
 
 const HealthCheckController = require('./controller/HealthCheckController');
 
-io.on('connection', (socket) => {
+const PORT = 3000;
+
+  
+
+socketIO.on('connection', (socket) => {
     console.log(`User connected!`)
+
+    socket.on('disconnet', () => {
+        console.log(`user disconnected`);
+    });
+
+    socket.on("hello", (params, callbackwiki) => {
+        console.log(`Hee nhooo hoooo heax ${JSON.stringify(params)}`);
+    })
 });
+
 
 app.use(`/hc`, HealthCheckController);
 
-server.listen(3000, () => {
-    console.log(`Server running on http://localhost:3000`);
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
